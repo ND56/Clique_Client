f0da9d6bee6d8230bf605a6a016be9d28d13778b
ND56 <ndrew56@gmail.com>
2018-03-29 07:47:08 -0400

    When you add a comment, the DOM updates with an appropriate edit button

--- assets/scripts/user/ui.js
+++ assets/scripts/user/ui.js
@@ -314,10 +314,14 @@ const timeOutMessage = () => {
 }
 
 const addCommentSuccess = (apiResponse) => {
+  const commentsArrLength = apiResponse.image.comments.length
+  const index = commentsArrLength - 1
+  console.log(apiResponse)
   $('#submit-comment-form').each(function () {
     this.reset()
   })
   $('#comments-wrapper').prepend('<div class="sample-comment" id="comment-template"><span id="new-comment">' + store.mostRecentComment + '</span><br /><span class="commentor-1">-</span><span class="commentor-1" id="new-commentor">' + store.user.email + '</span></div>')
+  $('#comment-template').append('<button class="btn btn-default edit-comment-button" data-id="' + apiResponse.image.comments[index][0] + '" id="' + apiResponse.image.comments[index][2] + '">Edit</button>')
 }
 
 const addCommentFailure = () => {
